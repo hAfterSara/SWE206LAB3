4d106747712a4cf6e9ccebbaf44a4a3460d09eda
unknown <Sa.alabkari@gmail.com>
2024-02-07 16:08:03 +0300

    Selection Sort

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;//searching for lowest index
+                }
+            }
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
     }
 }
