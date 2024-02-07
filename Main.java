dd3091a535434cbe2a16dddce15ba86719ec503d
unknown <Sa.alabkari@gmail.com>
2024-02-07 16:50:25 +0300

    Updated

--- Main.java
+++ Main.java
@@ -1,38 +1,23 @@
 public class Main {
-<<<<<<< HEAD
-    public static void main(String[] args){
-        public static void insertionSort(int array[]) {
-            int n = array.length;
-            for (int j = 1; j < n; j++) {
-                int key = array[j];
-                int i = j-1;
-                while ( (i > -1) && ( array [i] > key ) ) {
-                    array [i+1] = array [i];
-                    i--;
-                }
-                array[i+1] = key;
+    public static void insertionSort(int array[]) {
+        int n = array.length;
+        for (int j = 1; j < n; j++) {
+            int key = array[j];
+            int i = j-1;
+            while ( (i > -1) && ( array [i] > key ) ) {
+                array [i+1] = array [i];
+                i--;
             }
+            array[i+1] = key;
         }
-
     }
     public static void main(String a[]){
         int[] arr1 = {9,14,3,2,43,11,58,22};
-        System.out.println("Before Insertion Sort");
-        for(int i:arr1){
-            System.out.print(i+" ");
-        }
-        System.out.println();
-
-        insertionSort(arr1);//sorting array using insertion sort
-
-        System.out.println("After Insertion Sort");
-        for(int i:arr1){
-            System.out.print(i+" ");
-        }
     }
-}
-
-
+    public static void sort(int[] array){
+        insertionSort(array);
+    }
+    
     public static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             int index = i;
@@ -47,4 +32,4 @@ public class Main {
         }
     }
 }
->>>>>>> 4d106747712a4cf6e9ccebbaf44a4a3460d09eda
+
